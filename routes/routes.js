f191cad413396bcf111e7473e3c4615b0c082193
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-17 15:02:40 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 module.exports.newData = "New Data"
 
-module.exports.addJson =  page3: {
+module.exports.addJson =  {
                               title: "pag3", 
                               content:'../contents/pag3.ejs',
                               descr: 'empty.....',
