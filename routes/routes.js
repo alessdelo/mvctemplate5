cd5a79db058ff68dd7e46f4086ec6598bc945330
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-17 13:12:47 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,12 @@
+var data = JSON.parse(pageData);  //parse the JSON
+data.pag3.push({        //add the employee
+    title: "pag3", 
+    content:'../contents/pag3.ejs',
+    descr: 'empty.....',
+    nav: nav,
+    header: header,
+    footer: footer,
+    params: []
+});
 
+pageData = JSON.stringify(data);  //reserialize to JSON
