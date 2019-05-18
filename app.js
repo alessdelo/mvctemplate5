02c320c4bec9e81b0f0c11a6268187591c28bff6
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-18 06:17:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ var pageData = {
 
 // pageData.push(importData.addJson)
 
-var testData = {gino:"franco"}
-var text2 = {foo:"bar"}
+var testData = [{gino:"franco"}]
+var text2 = [{foo:"bar"}]
 testData.push(text2)
 console.log(testData);
 
