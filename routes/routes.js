07387c21386a15c3bde88e0ac6eb01132a556337
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-19 07:37:13 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,8 @@ module.exports.newData = "New Data"
 /*
 var text =  {
            title: "pag3", 
-           content:'../contents/pag3.ejs',
-           descr: 'empty.....',
+           content:"../contents/pag3.ejs",
+           descr: "empty.....",
            nav: nav,
            header: header,
            footer: footer,
