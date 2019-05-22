b23cd50029c64529b8f89340019f12bd572cc8d5
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-22 17:44:50 +0200

    Update page3.js

--- routes/page3.js
+++ routes/page3.js
@@ -1,8 +1,5 @@
 module.exports.newData = "New Data"
 
- // var text = {foo:"bar", pluto:"ernesto"}
-
-
 
 var text =  {
            title: "pag3", 
@@ -14,22 +11,6 @@ var text =  {
            params: []
           }
  
-
- 
- module.exports.addJson = text
+module.exports.addJson = text
    
 
-/*
-var 
-/*
-module.
-
-/*
-var data = JSON.parse(pageData);  //parse the JSON
-data.pag3.push({        //add the employee
-    title: "pag3", 
-    content:'../contents/pag3.ejs',
-    descr: 'empty.....',
-    nav: nav,
-    
-*/
