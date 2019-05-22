00918d937e13a4601d88666d353579bbba5d307d
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-22 07:02:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ pageData.items.forEach(function(value, index, array) {
     // `value` is the element itself (equivalent to `array[index]`)
     // `index` will be the index of the element in the array
     // `array` is a reference to the array itself (i.e. `data.items` in this case)
+    console.log(value)
 }); 
 
 
