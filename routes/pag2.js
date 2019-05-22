649d3fa3d89af15ec577af5ea09e492502760813
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-22 18:38:20 +0200

    Create pag2.js

---
+++ routes/pag2.js
@@ -0,0 +1,11 @@
+var text =  {
+           title: "pag2", 
+           content:"../contents/pag2.ejs",
+           descr: "empty.....",
+           nav: "",
+           header: "",
+           footer: "",
+           params: []
+          }
+ 
+module.exports.addJson = text
