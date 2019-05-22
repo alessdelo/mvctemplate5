ac3cae81060a38ee8fae2d0a1f0ee2615f69155a
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-22 18:39:03 +0200

    Create home.js

---
+++ routes/home.js
@@ -0,0 +1,11 @@
+var text =  {
+           title: "home", 
+           content:"../contents/home.ejs",
+           descr: "empty.....",
+           nav: "",
+           header: "",
+           footer: "",
+           params: []
+          }
+ 
+module.exports.addJson = text
