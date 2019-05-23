136e56928c8d5bfae8ea2f2bfdf8b774609a22f9
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-23 07:10:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-// const smartContractAddress = process.env.WALLET_ADDRESS
-
-// const Web3 = require('web3')
-// retrieves infura key from Heroku constants
-// Ropsten
-// const infuraRopstenKey = process.env.INFURA_ROPSTEN_KEY
-// Rinkeby
-// const infuraRinkebyKey = process.env.INFURA_RINKEBY_KEY
-
-// const keytest = "myKeyTest"
-
-// connect to Infura node
-// Ropsten
-// const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/' + infuraRopstenKey))
-// Rinkeby
-// const InfuraWeb3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/' + infuraRinkebyKey))
+// Import variable from heroku
+// const varFromHeroku = process.env.VAR_FROM_HEROKU
 
 var index = 'pages/index'
 // var pageData = {}
