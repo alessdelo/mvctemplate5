feccb7ee0dd18c91a06e4ebf8dc08a073bb8118a
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-28 07:57:24 +0200

    Update mongo0.js

--- models/mongo0.js
+++ models/mongo0.js
@@ -1,8 +1,10 @@
 // https://mongoosejs.com/docs/
 
-var mongoose = require("mongoose");
+var mongoose = require("mongoose")
 
 // a very basic mongoose schema
-var kittySchema = new mongoose.Schema({ name: String });
+var kittySchema = new mongoose.Schema({ 
+       name: String
+     })
 
-module.exports = mongoose.model("Kitty", kittySchema);
+module.exports = mongoose.model("Kitty", kittySchema)
