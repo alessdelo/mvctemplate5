21ffaac78a040faab64964b0fd81621f30c5e743
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-29 14:30:49 +0200

    Create kitty.js

---
+++ controllers/kitty.js
@@ -0,0 +1,16 @@
+/*
+exports.kitty_create = function (req, res) {
+    let kitty = new Kitty(
+        {
+            name: req.body.name
+        }
+    );
+
+    kitty.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Kitty Created successfully')
+    })
+};
+*/
