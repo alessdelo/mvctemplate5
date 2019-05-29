b9c93c9e96f23ef5e6137f6dd330b94b29b0fad5
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-29 22:54:42 +0200

    Update test.js

--- controllers/test.js
+++ controllers/test.js
@@ -2,3 +2,7 @@
 exports.test = function (req, res) {
     res.send('Greetings from the Test00 controller!!!')
 }
+
+exports.test1 = function (req, res) {
+    res.send('Greetings from the Test01 controller!!!')
+}
