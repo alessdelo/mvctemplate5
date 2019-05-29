7e3bb0c7c8adcf9e3641a1c1fd00dcf6f093bf0c
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-29 22:56:31 +0200

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -7,4 +7,8 @@ const test_controller = require('../controllers/test');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test0', test_controller.test);
+
+// 2nd test
+router.get('/test1', test_controller.test1);
+
 module.exports = router;
