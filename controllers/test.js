22c0de464c3f8590c4eb721f139bf7c0e36a1283
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-29 18:49:06 +0200

    Update test.js

--- controllers/test.js
+++ controllers/test.js
@@ -1,4 +1,4 @@
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
-    res.send('Greetings from the Test controller!')
+    res.send('Greetings from the Test00 controller!!!')
 }
