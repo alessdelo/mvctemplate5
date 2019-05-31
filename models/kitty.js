9802b62578adad8627702cbe480a5af46c4901f8
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-31 07:05:09 +0200

    Update kitty.js

--- models/kitty.js
+++ models/kitty.js
@@ -4,7 +4,8 @@ var mongoose = require("mongoose")
 
 // a very basic mongoose schema
 var kittySchema = new mongoose.Schema({ 
-       name: String
+       name: String,
+       age: String
      })
 
 module.exports = mongoose.model("Kitty", kittySchema)
