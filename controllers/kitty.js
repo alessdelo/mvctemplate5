829e104c108c9a2220a72c987d17bd2c47602a9d
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-05-31 07:06:20 +0200

    Update kitty.js

--- controllers/kitty.js
+++ controllers/kitty.js
@@ -1,14 +1,6 @@
-// const Kitty = require('../models/kitty')
-
-//Simple version, without validation or sanitation
-exports.test = function (req, res) {
-    res.send('Greetings from the Test controller!')
-}
+const Kitty = require('../models/kitty')
 
 
-
-
-/*
 exports.kitty_create = function (req, res) {
     let kitty = new Kitty(
         {
@@ -18,10 +10,10 @@ exports.kitty_create = function (req, res) {
 
     kitty.save(function (err) {
         if (err) {
-            return next(err);
+            return next(err)
         }
         res.send('Kitty Created successfully')
     })
-};
-*/
+}
+
 
