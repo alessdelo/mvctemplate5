c354b1ca0a14f42843882527fbd47dc8bde9924e
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-06-04 15:05:28 +0200

    Update randoms.js

--- helpers/randoms.js
+++ helpers/randoms.js
@@ -10,3 +10,32 @@ function makeid(length) {
 }
 
 // console.log(makeid(5));
+
+
+// ----------------------------------------
+
+function randAge() {
+        let age = Math.floor(Math.random() * 99)
+        
+        age = age.toString()
+  
+        return age       
+}
+
+// ----------------------------------------
+
+//random string 
+function randName(length) {
+   var result           = '';
+   var consonants       = 'bcdfghjklmnpqrstuvwxyz';
+   var vocals       = 'aeiou';
+   
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      if (i == 0 || i/2 == 0 ||) 
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+// console.log(randName(5));
