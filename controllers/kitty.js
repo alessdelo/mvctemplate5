93f84ce86830ff0c1d248a1cbfbec601091109f6
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-06-05 16:05:19 +0200

    Update kitty.js

--- controllers/kitty.js
+++ controllers/kitty.js
@@ -21,9 +21,28 @@ exports.kitty_create = function (req, res) {
 
 }
 
+// READ
+exports.kitty_details = function (req, res) {
+    Kitty.findById(req.params.id, function (err, kitten) {
+        if (err) return next(err)
+        res.send(kitten)
+    })
+}
+
+
+// UPDATE
 exports.kitty_update = function (req, res) {
     Kitty.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
         if (err) return next(err)
         res.send('Kitty udpated.')
+    })
+}
+
+
+// DELETE
+exports.kitty_delete = function (req, res) {
+    Kitty.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err)
+        res.send('Deleted successfully!')
     })
 }
