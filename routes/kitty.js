90b65c613c6bd141ed049632cca62754fe1cd21b
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-06-05 16:09:15 +0200

    Update kitty.js

--- routes/kitty.js
+++ routes/kitty.js
@@ -8,10 +8,10 @@ const kitty_controller = require('../controllers/kitty');
 // create
 router.get('/test/:name/:age', kitty_controller.kitty_create);
 // read
-router.get('/:id', product_controller.product_details);
+router.get('/test/:id', product_controller.product_details);
 // update
 router.put('/test/:id/update', kitty_controller.kitty_update);
 // delete
-router.delete('/:id/delete', kitty_controller.kitty_delete);
+router.delete('/test/:id/delete', kitty_controller.kitty_delete);
 
 module.exports = router;
