c22f7b98b31ded8d65fddef67fc58b0956959ceb
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-06-05 15:57:00 +0200

    Update kitty.js

--- routes/kitty.js
+++ routes/kitty.js
@@ -7,4 +7,5 @@ const kitty_controller = require('../controllers/kitty');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test/:name/:age', kitty_controller.kitty_create);
+router.put('/test/:id/update', kitty_controller.kitty_update);
 module.exports = router;
