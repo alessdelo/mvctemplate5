322f7df818f7cf6183a678706a2d3e5154aacc19
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-06-07 08:03:46 +0200

    Update debug.js

--- helpers/debug.js
+++ helpers/debug.js
@@ -3,6 +3,7 @@
 // **********
 
 // OBJECTS
+
  // printObject() -  converts an Object into a text and alerts
             function printObject(o) {
                 var out = '';
@@ -10,6 +11,17 @@
                   out += p + ': ' + o[p] + '\n';
                 }
                 alert(out);
+            }
+            
+// ------------------------
+
+// logObject() -  converts an Object into a text and writes it to the console
+            function printObject(o) {
+                var out = '';
+                for (var p in o) {
+                  out += p + ': ' + o[p] + '\n';
+                }
+                console.log(out);
             }
             
 // ------------------------
