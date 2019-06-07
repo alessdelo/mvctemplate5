92c172b84b924767c06290c05953a8eb2662382c
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-06-07 14:18:00 +0200

    Update kitty.js

--- controllers/kitty.js
+++ controllers/kitty.js
@@ -45,7 +45,9 @@ exports.kitty_update = function (req, res) {
                                                                                 ' name: ' + req.params.name + 
                                                                                 ' age: ' + req.params.age
                                                                                 )
-                            })
+                            },
+                            { useFindAndModify: false }
+                           )
 } // end kitty_update
 
 
