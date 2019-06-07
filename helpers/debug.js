176d289f3800f1c1d89ff7d9e92df97b9d10c449
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-06-07 08:00:57 +0200

    Create debug.js

---
+++ helpers/debug.js
@@ -0,0 +1,15 @@
+// **********
+// DEBUG
+// **********
+
+// PRINTOBJECT
+ // converts an Object into a text and alerts
+            function printObject(o) {
+                var out = '';
+                for (var p in o) {
+                  out += p + ': ' + o[p] + '\n';
+                }
+                alert(out);
+            }
+            
+// ------------------------
