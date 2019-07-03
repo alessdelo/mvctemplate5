cb7b61ab7689801d1717092bbe6a576c3767bed8
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-03 07:59:00 +0200

    Update kitty.js

--- controllers/kitty.js
+++ controllers/kitty.js
@@ -69,7 +69,7 @@ exports.kitty_lastitems = function (req, res) {
 
     
     Kitty.find(req.params.num, { useFindAndModify: false }, function (err, result) {
-// if (err) return next(err)
+ if (err) res.send(debugs.textFromObject(err)) // return next(err)
 // or if no items are found maybe
 // if (0 === result.length) return next(new NotFoundError))
    
