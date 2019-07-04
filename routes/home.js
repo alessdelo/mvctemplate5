abb1500f86eb72402d9b19cc9c26e56aa21cf642
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-04 14:41:09 +0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,13 @@
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const home_controller = require('../controllers/home');
+
+
+// create
+router.get('/', home_controller.home);
+
+
+
+
 /*
 var importVars = require('././vars.js')
 var theIndex = importVars.index
