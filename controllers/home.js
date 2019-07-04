e04c82a8eaf2e9e378d1a1d8ab3bf8cf72b02f01
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-04 14:27:08 +0200

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,15 @@
 const Home = require('../models/home')
 const debugs = require('../helpers/debugs.js')
 
+var home: {	 
+    title:'Three.js hello world',
+    content:'../contents/threehello.ejs',
+    nav: nav,
+    header: header,
+    footer: footer,
+    params: []
+  }
+
 exports.home = function (req, res) {
-        res.render(index,pageData.home)
+        res.render(index,home)
 }
