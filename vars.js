41903c8beec6a09b465c1a218cbfbf683108f096
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-06 13:44:16 +0200

    Update vars.js

--- vars.js
+++ vars.js
@@ -1,10 +1,3 @@
-/*
-var index = 'pages/index'
-var header = "../partials/header.ejs"
-var nav = "../partials/nav.ejs"
-var footer = "../partials/footer.ejs"
-*/
-
 var varsObj =  {
                 index: 'pages/index', 
                 header:"../partials/header.ejs",
