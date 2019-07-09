fb42b047e860bb346e7fd6ff93137552fa903415
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-09 06:58:57 +0200

    Update extractors.js

--- helpers/extractors.js
+++ helpers/extractors.js
@@ -1,1 +1,9 @@
+function getFields(input, field) {
+    var output = [];
+    for (var i=0; i < input.length ; ++i)
+        output.push(input[i][field]);
+    return output;
+}
 
+// returns [ 1, 3, 5 ]
+// var result = getFields(objArray, "foo");
