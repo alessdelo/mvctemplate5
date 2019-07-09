281536fea239107b90b4809d95880b545bb1e677
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-09 07:15:15 +0200

    Update extractors.js

--- helpers/extractors.js
+++ helpers/extractors.js
@@ -1,3 +1,19 @@
+
+/*
+I have JavaScript object array with the following structure:
+
+objArray = [ 
+             { foo: 1, bar: 2},
+             { foo: 3, bar: 4},
+             { foo: 5, bar: 6}
+           ];
+
+I want to extract a field from each object,
+and get an array containing the values, 
+for example field foo
+would give array [ 1, 3, 5 ].
+*/
+
 exports.getFieldsFromObj = function (input, field) {
     var output = [];
     for (var i=0; i < input.length ; ++i)
@@ -7,3 +23,5 @@ exports.getFieldsFromObj = function (input, field) {
 
 // returns [ 1, 3, 5 ]
 // var result = getFields(objArray, "foo");
+
+// -------------------------------------
