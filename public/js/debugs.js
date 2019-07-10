574a7f3e95ba55024f1a666f0adcde2a82970f63
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-10 15:38:33 +0200

    Create debugs.js

---
+++ public/js/debugs.js
@@ -0,0 +1,38 @@
+// **********
+// DEBUG
+// **********
+
+// OBJECTS
+
+ // printObject() -  converts an Object into a text and alerts
+          exports.printObject = function (o) {
+                var out = '';
+                for (var p in o) {
+                  out += p + ': ' + o[p] + '\n';
+                }
+                alert(out);
+            }
+            
+// ------------------------
+
+// logObject() -  converts an Object into a text and writes it to the console
+           exports.logObject = function (o) {
+                var out = '';
+                for (var p in o) {
+                  out += p + ': ' + o[p] + '\n';
+                }
+                console.log(out);
+            }
+            
+// ------------------------
+
+// textFromObject() -  returns a text from an Object
+            exports.textFromObject = function (o) {
+                var out = '';
+                for (var p in o) {
+                  out += p + ': ' + o[p] + '\n';
+                }
+                return out;
+            }
+            
+// ------------------------
