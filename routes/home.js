d18da56c64d150455b2fd586393d55b002c32d34
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-11 15:16:37 +0200

    Update and rename home2.js to home.js

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const home2_controller = require('../controllers/home2');
 
-router.get('/home2', home2_controller.home2);
+router.get('/home', home2_controller.home2);
 
 router.get('/', home2_controller.home2);
 
