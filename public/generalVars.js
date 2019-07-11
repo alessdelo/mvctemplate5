f26cfb3302427ddc466ac1fbe4233f32ddb8f97b
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-11 15:01:02 +0200

    Update generalVars.js

--- public/generalVars.js
+++ public/generalVars.js
@@ -1,4 +1,8 @@
-var index = 'pages/index'
-var header = "../partials/header.ejs"
-var nav = "../partials/nav.ejs"
-var footer = "../partials/footer.ejs"
+var varsObj =  {
+                index: 'pages/index', 
+                header:"../partials/header.ejs",
+                nav: "../partials/nav.ejs",
+                footer: "../partials/footer.ejs"
+               }
+
+module.exports.index = varsObj
