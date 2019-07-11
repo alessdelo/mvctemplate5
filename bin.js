bd80a80b444b85898c858298422f8798dc03c607
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-11 07:05:55 +0200

    Update bin.js

--- bin.js
+++ bin.js
@@ -1,4 +1,29 @@
-  _------------------------------------------
+<p class="lead">The Home 2</p>
+  <hr class="my-4">
+  <p>This is an amazing page!!!</p>
+  <script> alert('home2'); </script>
+  <hr class="my-4">
+
+<script type="text/javascript" src="/static/js/extractors.js"></script>
+<script type="text/javascript" src="/static/js/debugs.js"></script>
+
+<% strMyVar = JSON.stringify(myVar, null, 4); %>
+<%= strMyVar %>
+<% var passEjs = strMyVar; %>
+<% var passEjsObj = myVar; %>
+
+<script>
+var getPassEjs = <%- passEjs %>;
+</script>
+
+<script>
+  // How to extract values ftom getPassEjs
+  alert("getPassEjs = " + getPassEjs);
+  alert(getPassEjs[0]);
+  alert(getPassEjs[0]["puppyTestInfos"]);
+
+
+ _------------------------------------------
   
   Kitty.
         find({"name" : /.*a.*/i} ).
