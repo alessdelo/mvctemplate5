1bf3880d1d6f3becc47506b4396eb191d75c91d0
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-12 14:35:22 +0200

    Create form1.js

---
+++ routes/form1.js
@@ -0,0 +1,21 @@
+    
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const the_controller = require('../controllers/form1');
+
+
+// create
+router.get('/create/:name/:age', the_controller.create);
+// read
+router.get('/read/:id', the_controller.read);
+// update
+router.get('/update/:id/:name/:age', the_controller.update);
+// delete
+router.get('/delete/:id', the_controller.delete);
+// extracts last n. items
+router.get('/lastitems/:num', the_controller.lastitems);
+
+
+module.exports = router;
