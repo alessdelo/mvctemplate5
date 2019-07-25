03c497395da122a1554226a66525bb78c7c1aef2
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-25 18:59:22 +0200

    Update doms.js

--- public/js/doms.js
+++ public/js/doms.js
@@ -4,12 +4,28 @@
 // changes the target color, giving queryselector (#div, .class, etc...) and color code
 
    function changeColor(qSelector, theColor){
-          let a = document.querySelector(qSelector);
-          a.style.color = theColor;
+          let x = document.querySelector(qSelector);
+          x.style.color = theColor;
    }
    
 // example
 // <p class="name" onclick="changeColor('#theTarget0','red')">Change Color</p>
 // <p id="theTarget0">Color Target</p>
+
+// --------------------------------------------------------------------------------------
+
+// *************
+// CHANGE TEXT
+// *************
+// changes the target text, giving queryselector (#div, .class, etc...) and text
+
+   function changeText(qSelector, theText){
+          let x = document.querySelector(qSelector);
+          x.innerHTML = theText;
+   }
+   
+// example
+// <p class="name" onclick="changeText('#theTargetText','I am The new Text!!!')">Change Text</p>
+// <p id="theTargetText">Text Target</p>
 
 // --------------------------------------------------------------------------------------
