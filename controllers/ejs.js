1589eb5dbe4f21555e7cd929606dda697289d27f
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-25 23:10:37 +0200

    Create ejs.js

---
+++ controllers/ejs.js
@@ -0,0 +1,22 @@
+var importVars = require('../vars.js')
+var theIndex = importVars.index
+
+exports.toDo = function (req, res) {
+
+        var theParams = {
+                 "name": req.params.name,
+                 "age": req.params.age
+                 }
+    
+        var text =  {
+                 title: "ejs", 
+                 content:"../contents/ejs.ejs",
+                 descr: "empty.....",
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+        res.render(theIndex.index,text)
+      
+}  end toDo
