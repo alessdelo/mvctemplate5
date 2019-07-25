b1b02afdb14da0509c17bfd342f25280c51b5983
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-07-25 19:10:55 +0200

    Update doms.js

--- public/js/doms.js
+++ public/js/doms.js
@@ -1,7 +1,7 @@
 // *************
 // CHANGE COLOR
 // *************
-// changes the target color, giving queryselector (#div, .class, etc...) and color code
+// changes the target color, giving queryselector (#div, .class, etc...between quotation marks) and color code (between quotation marks)
 
    function changeColor(qSelector, theColor){
           let x = document.querySelector(qSelector);
@@ -17,9 +17,9 @@
 // *********************************
 // CHANGE TEXT giving QuerySelector
 // *********************************
-// changes the target text, giving queryselector (#div, .class, etc...) and text
+// changes the target text, giving queryselector (#div, .class, etc... between quotation marks) and text (between quotation marks)
 
-   function changeText(qSelector, theText){
+   function changeTextByQsel(qSelector, theText){
           let x = document.querySelector(qSelector);
           x.innerHTML = theText;
    }
@@ -27,5 +27,21 @@
 // example
 // <p class="name" onclick="changeText('#theTargetText','I am The new Text!!!')">Change Text</p>
 // <p id="theTargetText">Text Target</p>
+
+// --------------------------------------------------------------------------------------
+
+// *********************************
+// CHANGE TEXT giving id name
+// *********************************
+// changes the target text, giving id name (between quotation marks and without #) and text (between quotation marks)
+
+   function changeTextByDiv(theID, theText){
+          let x = document.getElementById(theID);
+          x.innerHTML = theText;
+   }
+   
+// example
+// <p class="name" onclick="changeText('myDiv','I am The new Text!!!')">Change Text</p>
+// <p id="myDiv">Text Target</p>
 
 // --------------------------------------------------------------------------------------
