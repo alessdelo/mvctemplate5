eecf29ff015e1b9cd8c462acd591b3e497daa21f
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-06 22:03:34 +0200

    Update createp.js

--- controllers/form2/createp.js
+++ controllers/form2/createp.js
@@ -1,9 +1,12 @@
 // CREATES A NEW RECORD FROM DATA SENT VIA POST
-const theModel = require('../../models/kitty')
-const debugs = require('../../helpers/debugs.js')
-const randoms = require('../../helpers/randoms.js')
 
-var importVars = require('../vars.js')
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/kitty')
+const debugs = require(mainFolder + 'helpers/debugs.js')
+const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
 var theIndex = importVars.index
 
 var theTitle = "Form 2 (Record Created)"
