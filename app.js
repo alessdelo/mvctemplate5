f6ad4c57162aa9cec45d211eb8257f8cb133810a
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-07 07:19:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ var app = express()
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({extended: true}))
   // app.use(multer()); // for parsing multipart/form-data
+  app.set('controllers', path.join(__dirname, 'controllers'))
 
   /*
   // logs the http request
