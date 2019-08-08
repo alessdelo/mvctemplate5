e9ef28fa34a6b89b84d99f5d5d0ac4e33e167849
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-08 20:19:06 +0200

    Update search.js

--- controllers/form2/search.js
+++ controllers/form2/search.js
@@ -1,1 +1,19 @@
+// CREATES A RECORD (GET REQUEST)
+
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/kitty')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Form2 (Search)"
+var theUrl = "../contents/form2/search.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+
 MyModel.find({ name: 'john', age: { $gte: 18 }}, function (err, docs) {});
