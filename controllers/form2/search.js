4652b3ceb25e9af1286d4a361e30b7ad61d066bd
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-09 08:51:34 +0200

    Update search.js

--- controllers/form2/search.js
+++ controllers/form2/search.js
@@ -20,7 +20,11 @@ var theDescr = "empty....."
 
 exports.search = function (req, res) {
    
-   theModel.find({ name: 'Dazaq'}, function (err, docs) {
+   let theQuery = {}
+   
+   if (req.body.id == "") {}
+   
+   theModel.find({ _id name: 'Dazaq'}, function (err, docs) {
    
    // theModel.find({ name: 'Dazaq', age: { $all:["age"] }}, function (err, docs) {
 
