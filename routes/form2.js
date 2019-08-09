134332deb4db85c2fcf69ce7ea47501bc0b73dc6
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-09 08:28:52 +0200

    Update form2.js

--- routes/form2.js
+++ routes/form2.js
@@ -1,45 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
-/*
-
-// Require the controllers WHICH WE DID NOT CREATE YET!!
-const the_controller = "../controllers/form2";
-
-// create record (get request)
-
-const the_createg = require(the_controller + "createg" + ".js");
-
-router.get('/createg', the_createg.createg);
-
-*/
-
-
-/*
-// Require the controllers WHICH WE DID NOT CREATE YET!!
-// const the_controller = "../controllers/form2";
-const the_controller = "controllers/form2";
-
-
-const contr = [];
-
-// create record (get request)
-
-var contrName = "createg";
-
-contr[contrName] = require(the_controller + "/" + contrName + ".js");
-
-router.get('/' + contrName, contr[contrName][contrName]);
-
-*/
-
-
-
-// Require the controllers WHICH WE DID NOT CREATE YET!!
 const the_controller = "../controllers/form2/";
-
-
 
 const contr = [
                 {"name": "createg", "req": "get", "params": "" },
@@ -72,53 +34,8 @@ for (i = 0; i < contr.length; i++) {
           // example = edit:  router.get('/edit/:id', the_controller);
           router.get('/' + contrName + contrParams, contr[contrName][contrName]);
 
-    }
+    } // end if-else
 
-}
-
-
-
-
-// create record (get request)
-
-
-
-
-
-
-
-/*
-
-// create
-    // with data
-    router.post('/createp', the_controller.createp);
-    // router.post('/createp', the_controller.form1createp);
-    // with no data
-    router.get('/create', the_controller.create);
-  
-
-// read
-router.get('/read/:id', the_controller.read);
-// UPDATE
-// edit
-router.get('/edit/:id', the_controller.edit);
-// update
-router.get('/update/:id/:name/:age', the_controller.update);
-// update post
-router.post('/updatep', the_controller.updatep);
-// update get from form
-router.get('/updatep', the_controller.updatep);
-
-
-
-// delete
-router.get('/delete/:id', the_controller.delete);
-router.get('/deletep/:id', the_controller.deletep);
-
-// extracts last n. items
-router.get('/lastitems/:num', the_controller.lastitems);
-
-*/
-
+} // end for
 
 module.exports = router;
