55a9c483e52932d5e09fbc848d8db456ee41d9c1
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-10 12:12:52 +0200

    Update form1main.js

--- routes/form1main.js
+++ routes/form1main.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
 
+// takes variables and functions from controllers/home.js
+const the_controller = require('../controllers/form1main');
+
+router.get('/', the_controller.toDo);
+
+module.exports = router;
