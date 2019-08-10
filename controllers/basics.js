9b070652eef8bb89c395c207bee81c865110e6a9
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-10 11:47:19 +0200

    Update basics.js

--- controllers/basics.js
+++ controllers/basics.js
@@ -19,13 +19,13 @@ exports.toDo = function (req, res) {
 
 
     // Get the count of all users
-    User.count().exec(function (err, count) {
+    theModel.count().exec(function (err, count) {
 
       // Get a random entry
       var random = Math.floor(Math.random() * count)
 
       // Again query all users but only fetch one offset by our random #
-      User.findOne().skip(random).exec(
+      theModel.findOne().skip(random).exec(
         function (err, result) {
           // Tada! random user
           // console.log(result)
