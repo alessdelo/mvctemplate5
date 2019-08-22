daa41b6e2de9770d7947932ea10de2a6c5646e6e
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-22 12:07:40 +0200

    Update form3maps.js

--- routes/form3maps.js
+++ routes/form3maps.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const the_controller = "../controllers/form3maps/";
+const the_controller = "../controllers/form2/";
 
 const contr = [
-                {"name": "create", "req": "get", "params": "/:name/:description" }
-              ];
-              
- /*
-
+                {"name": "createg", "req": "get", "params": "" },
+                {"name": "createp", "req": "post", "params": "" },
                 {"name": "readg", "req": "get", "params": "/:id" },
                 {"name": "edit", "req": "get", "params": "/:id" },
                 {"name": "updatep", "req": "post", "params": "" },
                 {"name": "search", "req": "post", "params": "" },
                 {"name": "recordlist", "req": "get", "params": "/:num" },
                 {"name": "recordlistpag", "req": "get", "params": "/:page/:limit/:pagnum" }
- */
-              
-              
- /*
- 
+              ];
+
 var i;
 
 for (i = 0; i < contr.length; i++) { 
@@ -28,13 +22,13 @@ for (i = 0; i < contr.length; i++) {
     let contrReq = contr[i]["req"];
     let contrParams = contr[i]["params"];
 
-    // example: const the_controller = require("../controllers/form3maps/edit.js");
+    // example: const the_controller = require("../controllers/form2/edit.js");
     contr[contrName] = require(the_controller + contrName + ".js");
 
     if(contrReq == "post") {
       
            // example = edit:  router.post('/edit', the_controller);
-            router.post('/' + contrName, contr[contrName][contrName]);
+           router.post('/' + contrName, contr[contrName][contrName]);
 
     } else {
       
@@ -44,12 +38,5 @@ for (i = 0; i < contr.length; i++) {
     } // end if-else
 
 } // end for
-
-*/
-
-// debug
-// const the_controller = require("../controllers/form3maps/create.js")
-router.get('/create/:name/:description', the_controller);
-
 
 module.exports = router;
