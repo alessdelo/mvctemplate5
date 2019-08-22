3a336f7d7edc2d2bbaa10c430ca4751c3ee28759
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-22 08:34:02 +0200

    Create createg.js

---
+++ controllers/form3maps1/createg.js
@@ -0,0 +1,53 @@
+// CREATES A RECORD (GET REQUEST)
+
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/kitty')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Form 3 Maps (Create)"
+var theUrl = "../contents/form3maps/createg.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+exports.createg = function (req, res) {
+
+    let theSchema = new theModel(
+        {
+            name: req.params.name,
+            description: req.params.description,
+            
+            
+        }
+    )
+
+    theSchema.save(function (err) {
+        if (err) {
+            return next(err)
+        }
+        
+        var theParams = {
+                 "name": req.params.name,
+                 "age": req.params.age
+                 };
+
+        var text =  {
+                 title: theTitle, 
+                 content:theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+        
+        res.render(theIndex.index,text)
+        
+    }) // end save
+
+} // end create
