aa37c468c810800f98751f3535b9093551ffb2ce
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-24 15:42:42 +0200

    Create createe.js

---
+++ controllers/form3maps/createe.js
@@ -0,0 +1,37 @@
+// CREATES A RECORD (GET REQUEST)
+
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/maps')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Form 3 maps (Create)"
+var theUrl = "../contents/form3maps/create.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+// change this!!!!!!
+exports.create = function (req, res) {
+
+        var text =  {
+                 title: theTitle, 
+                 content:theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: []
+                }
+        
+        // debug
+        res.send(text)
+        // res.render(theIndex.index,text)
+        
+    }) // end save
+
+} // end create
