c280fa99e7e5569ce339e9d09d348db1427b0488
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-27 15:25:38 +0200

    Create createm.js

---
+++ controllers/form3maps/createm.js
@@ -0,0 +1,35 @@
+// CREATES A RECORD (GET REQUEST)
+
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/maps')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Form 3 maps (with OL)"
+var theUrl = "../contents/form3maps/createm.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+// change this!!!!!!
+exports.create = function (req, res) {
+
+        var text =  {
+                 title: theTitle, 
+                 content:theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: []
+                }
+        
+        // debug
+        // res.send(text)
+        res.render(theIndex.index,text)
+        
+} // end create
