f92308a216bf346a3b8e759d54fd1d794b126f62
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-27 08:16:56 +0200

    Update jquery.js

--- controllers/tests/jquery.js
+++ controllers/tests/jquery.js
@@ -1,1 +1,37 @@
+var mainFolder = "../../";
 
+// const theModel = require(mainFolder + 'models/maps')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Jquery tests"
+var theUrl = "../contents/tests/jquery.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+exports.jquery = function (req, res) {
+  
+    
+       
+       var theParams = {
+                 
+                 }
+           
+        var text =  {
+                 title: theTitle, 
+                 content: theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+        
+        // debug
+        //res.send(text)
+        
+        res.render(theIndex.index,text)
+
+} // end export.
