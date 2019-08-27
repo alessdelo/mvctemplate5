ebf3a756a7dae2daa71ec30f526092b6e29bfe0f
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-27 08:26:17 +0200

    Update tests.js

--- routes/tests.js
+++ routes/tests.js
@@ -1,1 +1,36 @@
+const express = require('express');
+const router = express.Router();
 
+const the_controller = "../controllers/tests/";
+
+const contr = [
+                {"name": "jquery", "req": "get", "params": "" }
+              ];
+
+
+var i;
+
+for (i = 0; i < contr.length; i++) { 
+   
+    let contrName = contr[i]["name"];
+    let contrReq = contr[i]["req"];
+    let contrParams = contr[i]["params"];
+
+    // example: const the_controller = require("../controllers/form2/edit.js");
+    contr[contrName] = require(the_controller + contrName + ".js");
+
+    if(contrReq == "post") {
+      
+           // example = edit:  router.post('/edit', the_controller);
+           router.post('/' + contrName, contr[contrName][contrName]);
+
+    } else {
+      
+          // example = edit:  router.get('/edit/:id', the_controller);
+          router.get('/' + contrName + contrParams, contr[contrName][contrName]);
+
+    } // end if-else
+
+} // end for
+
+module.exports = router;
