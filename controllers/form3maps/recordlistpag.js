16673ab9fb05ca0c78a3cfd3761339f18daac273
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-28 16:46:26 +0200

    Update recordlistpag.js

--- controllers/form3maps/recordlistpag.js
+++ controllers/form3maps/recordlistpag.js
@@ -24,7 +24,7 @@ exports.recordlistpag = function (req, res) {
     var limit = parseInt(req.params.limit) || 10
     
     theModel.find()
-                    .sort({time:-1})
+                    .sort({time.$date:-1})
                     .limit(limit)
                     .skip(page * limit)
     
