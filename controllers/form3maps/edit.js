fe81522b14334a835977fe49d8244ae673b607cc
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-08-31 16:50:33 +0200

    Create edit.js

---
+++ controllers/form3maps/edit.js
@@ -0,0 +1,40 @@
+// EDITS A RECORD FOR UPDATING - (GET REQUEST)
+
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/maps')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Form 3 - maps (Edit Record)"
+var theUrl = "../contents/form3maps/edit.ejs"
+var theDescr = "empty....."
+
+
+// --------------------------------------
+
+exports.edit = function (req, res) {
+    theModel.findById(req.params.id, function (err, result) {
+        if (err) return next(err)
+        
+                // res.send(result)
+                
+                var theParams = {result}
+       
+                var text =  {
+                         title: theTitle, 
+                         content: theUrl,
+                         descr: theDescr,
+                         nav: theIndex.nav,
+                         header: theIndex.header,
+                         footer: theIndex.footer,
+                         params: [theParams]
+                        }
+                // debugs
+                res.send(text)
+                // res.render(theIndex.index,text)
+    })
+}
