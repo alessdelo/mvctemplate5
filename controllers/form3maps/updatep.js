670e8609be34da63d64f925013067a6e6ca94984
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-09-03 19:17:03 +0200

    Update updatep.js

--- controllers/form3maps/updatep.js
+++ controllers/form3maps/updatep.js
@@ -34,8 +34,8 @@ exports.updatep = function (req, res) {
     
                             function (err, result) {
 
-                            if (err)  res.send(err + " - - - " + theReq.id + " - " + theReq.name + " - " + theReq.age  )                            
-                            // if (err) return next(new Error('There was an error'))
+                            // if (err)  res.send(err + " - - - " + theReq.id + " - " + theReq.name + " - " + theReq.age  )                            
+                             if (err) return next(new Error('There was an error'))
 
 
                                                     /* 
@@ -50,7 +50,13 @@ exports.updatep = function (req, res) {
                                                     // debug
                                                     // res.send(req.body)
 
-                                                    // var theParams = {"id":req.body.id, "name": req.body.name, "age": req.body.age}
+                                                    // var theParams = {"id":req.body.id,
+                                                                        "name": req.body.name,
+                                                                        "name": req.body.name,
+                                                                        "name": req.body.name,
+                                                                        "name": req.body.name,
+                                                                        "name": req.body.name,
+                                                                       }
                                                     
                                                     var theParams = theReq
                                                            
@@ -65,8 +71,9 @@ exports.updatep = function (req, res) {
                                                                 }
                                                     // debug
                                                     //res.send(text)
+                                                    res.send(result)
                                                    
-                                                      res.render(theIndex.index,text)
+                                                    // res.render(theIndex.index,text)
 
 
                             } )
