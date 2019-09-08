21a59605d287edab4f64d7276c56e794ac0a2c23
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-09-08 07:07:25 +0200

    Update updatep.js

--- controllers/form3maps/updatep.js
+++ controllers/form3maps/updatep.js
@@ -74,9 +74,9 @@ exports.updatep = function (req, res) {
 
  // debug
                                                     //res.send(text)
-                                                    // res.send(result)
+                                                    res.send(result)
                                                    
-                                                    res.render(theIndex.index,text)
+                                                    // res.render(theIndex.index,text)
 
 
                             } )
