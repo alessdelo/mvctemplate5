097ad51bea9d637f4839c63a26c0f4197bdc0d23
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-09-11 18:34:56 +0200

    Update OL.js

--- public/js/OL.js
+++ public/js/OL.js
@@ -1,3 +1,22 @@
+// CENTERS MAP TO GIVEN COORDINATRS
+// parameters: coordinate x, coordinate y, ol map element
+
+function doPan(coordx, coordy, theMap) {
+              
+                var view = new ol.View({
+                  // the view's initial state
+                  center: theMap.getView().getCenter(),
+                  zoom: 6
+                });
+		
+		// when we set the new location, the map will pan smoothly to it
+		theMap.getView().setCenter([coordx, coordy]);
+		
+}
+
+// ----------------------------
+
+
 function fromMercatorToWGS84(feature) {
 		var coords = feature.getGeometry().getCoordinates();
 		 var coordsWGS84 = ol.proj.transform([coords[1], coords[0]], 'EPSG:900913','EPSG:4326');
