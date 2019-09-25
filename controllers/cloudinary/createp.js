5477b227eb40fa774066b2a8e3851bb678562222
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-09-25 15:05:35 +0200

    Update createp.js

--- controllers/cloudinary/createp.js
+++ controllers/cloudinary/createp.js
@@ -16,7 +16,7 @@ cloudinary.config({
 
 var mainFolder = "../../";
 
-// const theModel = require(mainFolder + 'models/maps')
+const theModel = require(mainFolder + 'models/cloudinary')
 // const debugs = require(mainFolder + 'helpers/debugs.js')
 // const randoms = require(mainFolder + 'helpers/randoms.js')
 
@@ -40,6 +40,10 @@ exports.createp = function (req, res) {
           // Create a post model
           // by assembling all data as object
           // and passing to Model instance
+          
+          
+           /*
+           
           var post = new Model({
               title: req.body.title,
               description: req.body.description,
@@ -48,6 +52,8 @@ exports.createp = function (req, res) {
               image: result.url
               image_id: result.public_id
           });
+          
+         
           // Persist by saving
           post.save(function (err) {
               if(err){
@@ -57,8 +63,57 @@ exports.createp = function (req, res) {
               res.redirect('/');
           });
       });
+      
+      */
     
     // -----------------------------
+    
+     let theSchema = new theModel(
+        {
+             title: req.body.title,
+              description: req.body.description,
+              created_at: new Date(),
+              // Store the URL in a DB for future use
+              image: result.url
+              image_id: result.public_id  
+        }
+    )
+
+    theSchema.save(function (err, result) {
+        if (err) {
+            return next(err)
+        }
+      
+       // debug
+       // res.send(result)
+       // res.send(result.id);
+       
+       var theParams = {
+                 "id": result.id,
+                 "title": result.title,
+                 "created_at": result.created_at,
+                 "image": result.image,
+                 "image_id": result.image_id
+                 }
+           
+        var text =  {
+                 title: theTitle, 
+                 content: theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+        
+        // debug
+        res.send(text)
+        
+        // res.render(theIndex.index,text)
+      
+    }) // end save
+    
+    // ------------------------------
 
         var text =  {
                  title: theTitle, 
