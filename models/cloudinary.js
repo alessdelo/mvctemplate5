41f7823818c6eb33d5d3d19ad8413f9a89cb2afe
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-09-25 15:08:29 +0200

    Update cloudinary.js

--- models/cloudinary.js
+++ models/cloudinary.js
@@ -4,21 +4,11 @@ var mongoose = require("mongoose")
 
 // a very basic mongoose schema for locations
 var cloudinarySchema = new mongoose.Schema({
-                                  name: String,
-                                  description: String,
-                                  time : { type : Date, default: Date.now },
-                                  rate: Number,
-                                  loc: {
-                                         type: {
-                                                type: String,
-                                                default: "Point"
-                                               },
-                                         coordinates: {
-                                                type: [Number]
-                                              }
-                                       }   
-                                }, { collection: 'mvcmaps1'})
-
-mapsSchema.index({ loc: '2dsphere'})
+                                                title: String,
+                                                description: String,
+                                                image: String,
+                                                image_id: String,
+                                                created_at: Date
+                                            })
 
 module.exports = mongoose.model("Cloudinary", cloudinary)
