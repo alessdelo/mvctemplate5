39ca5ed183f808de3fa1a77881fe8826e076c7cb
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-09-28 13:22:40 +0200

    Update cloudinary.js

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -5,6 +5,7 @@ var multipartMiddleware = multipart();
 
 const the_controller = "../controllers/cloudinary/";
 
+
 const contr = [
                 {"name": "form1", "req": "get", "params": "", "middleware": "" },
                 {"name": "createp", "req": "post", "params": "", "middleware": multipartMiddleware },
