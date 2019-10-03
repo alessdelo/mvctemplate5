a0ed76dfb6054d1c1b25f6dc8ad534c6c06a8499
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-03 14:07:47 +0200

    Update updatep.js

--- controllers/cloudinary/updatep.js
+++ controllers/cloudinary/updatep.js
@@ -4,16 +4,16 @@
 
 // var cloudinary = require('cloudinary').v2
 
-
-// Configure Cloudinary
-// with credentials available on
-// your Cloudinary account dashboard
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
-})
-
+/*
+  // Configure Cloudinary
+  // with credentials available on
+  // your Cloudinary account dashboard
+  cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  })
+*/
 
 // UPDATES A RECORD (POST REQUEST)
 
