66cad426dfe4d11cb9ec24f2ce4d4e15b7759dd8
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-03 14:06:16 +0200

    Create cloudinary.js

---
+++ configs/cloudinary.js
@@ -0,0 +1,8 @@
+// Configure Cloudinary
+// with credentials available on
+// your Cloudinary account dashboard
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+})
