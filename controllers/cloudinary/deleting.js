212c4ef13acb42bde79ad7ae91140213b9d74231
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-04 14:42:51 +0200

    Update deleting.js

--- controllers/cloudinary/deleting.js
+++ controllers/cloudinary/deleting.js
@@ -7,14 +7,14 @@ const theModel = require(mainFolder + 'models/cloudinary')
 var importVars = require(mainFolder + 'vars.js')
 var theIndex = importVars.index
 
-var theTitle = "Form 1 - cloudinary (Edit Record)"
-var theUrl = "../contents/cloudinary/edit.ejs"
+var theTitle = "Cloudinary (Record to Delete)"
+var theUrl = "../contents/cloudinary/deleting.ejs"
 var theDescr = "empty....."
 
 
 // --------------------------------------
 
-exports.edit = function (req, res) {
+exports.deleting = function (req, res) {
        
     theModel.findById(req.params.id, function (err, result) {
         if (err) return next(err)
@@ -33,7 +33,7 @@ exports.edit = function (req, res) {
                          params: [theParams]
                         }
                 // debugs
-                // res.send(text)
-              res.render(theIndex.index,text)
+                res.send(text)
+             // res.render(theIndex.index,text)
     })
 }
