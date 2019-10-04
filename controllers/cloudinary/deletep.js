89f4503d3a2e6d7a996dcb13ded8dc4bd63a84a6
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-04 07:07:24 +0200

    Update deletep.js

--- controllers/cloudinary/deletep.js
+++ controllers/cloudinary/deletep.js
@@ -1,2 +1,48 @@
 cloudinary.v2.uploader.destroy('sample', function(error,result) {
   console.log(result, error) });
+
+
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/cloudinary')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Cloudinary: just deleted record"
+var theUrl = "../contents/cloudinary/deletep.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+exports.deletep = function (req, res) {
+    theModel.findByIdAndRemove(req.params.id, { useFindAndModify: false }, function (err, result) {
+        if (err) return next(err)
+        // res.send(result)
+        // res.send('item id: ' + req.params.id + ' Deleted successfully!')
+        // res.send( debugs.textFromObject(result) )
+                                                     
+                                                    var theParams = result
+       
+       
+                                                    
+                                                    var text =  {
+                                                                 title: theTitle, 
+                                                                 content: theUrl,
+                                                                 descr: theDescr,
+                                                                 nav: theIndex.nav,
+                                                                 header: theIndex.header,
+                                                                 footer: theIndex.footer,
+                                                                 params: [theParams]
+                                                                }
+                                                    
+                                                   // debug
+                                                   // res.send(text)
+                                                    
+                                                    res.render(theIndex.index,text)
+
+
+    })
+}
