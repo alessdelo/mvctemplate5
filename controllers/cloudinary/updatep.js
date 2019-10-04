eb467396443058bbf3ffe07510a068ace08769cd
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-04 19:41:26 +0200

    Update updatep.js

--- controllers/cloudinary/updatep.js
+++ controllers/cloudinary/updatep.js
@@ -77,7 +77,7 @@ exports.updatep = function (req, res) {
 
                                                     // var theParams = {"id":req.body.id, "name": req.body.name, "age": req.body.age}
                                                     
-                                                    var theParams = result
+                                                    var theParams = theReq
                                                            
                                                     var text =  {
                                                                  title: theTitle, 
