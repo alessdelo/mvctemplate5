c549a4373ceda445fb97f267ea0dd3a34015cb97
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-08 17:51:14 +0200

    Update cloudinary.js

--- configs/cloudinary.js
+++ configs/cloudinary.js
@@ -1,8 +1,12 @@
 // Configure Cloudinary
 // with credentials available on
 // your Cloudinary account dashboard
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
-})
+cloudyConfig ={ 
+                cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+                api_key: process.env.CLOUDINARY_API_KEY, 
+                api_secret: process.env.CLOUDINARY_API_SECRET 
+              };
+
+
+// export it
+exports.cloudyConfig = cloudyConfig;
