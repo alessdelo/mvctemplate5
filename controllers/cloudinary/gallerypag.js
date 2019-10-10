a8bd40f7cd9ccdcc7408ac68823be93d5e71d750
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-10 14:27:37 +0200

    Create gallerypag.js

---
+++ controllers/cloudinary/gallerypag.js
@@ -0,0 +1,45 @@
+// EXTRACTS RECORDS FOR THE PHOTO-GALLERY
+
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/cloudinary')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Photo Gallery with Pagination"
+var theUrl = "../contents/cloudinary/gallerypag.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------------------------------------
+
+                   //  
+
+exports.gallerypag = function (req, res) {   
+    
+    theModel.find()
+                    .sort({created_at:-1}) 
+    
+                    .exec(function(err, result) {
+                        theModel.count().exec(function(err, count) {
+                            
+                             var theParams = {result}
+       
+                            var text =  {
+                                     title: theTitle, 
+                                     content: theUrl,
+                                     descr: theDescr,
+                                     nav: theIndex.nav,
+                                     header: theIndex.header,
+                                     footer: theIndex.footer,
+                                     params: [theParams]
+                                    }
+                
+                            // debug
+                            // res.send(text)
+                             res.render(theIndex.index,text)
+
+                        })
+                    })
+    
+} // end .exports
