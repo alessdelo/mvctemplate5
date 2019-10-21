21f69e0657940c84e13e055a19e6f06a3655d44d
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-21 19:47:30 +0200

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,7 +1,7 @@
-// Configure Cloudinary
+// CLOUDINARY
 // with credentials available on
 // your Cloudinary account dashboard
-cloudyConfig ={ 
+cloudyConfig = { 
                 cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
                 api_key: process.env.CLOUDINARY_API_KEY, 
                 api_secret: process.env.CLOUDINARY_API_SECRET 
@@ -10,3 +10,15 @@ cloudyConfig ={
 
 // export it
 exports.cloudyConfig = cloudyConfig;
+
+// JWT
+// with credentials available on
+// your Cloudinary account dashboard
+jwtConfig = {
+    "connectionString": "mongodb://localhost/node-mongo-registration-login-api",
+    "secret": "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING"
+};
+
+// export it
+exports.jwtConfig = jwtConfig;
+
