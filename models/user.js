081481b724a3600561b6902d09a6bdead66bd8f8
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-26 20:35:34 +0200

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose")
+
+// a very basic mongoose schema for locations
+var userSchema = new mongoose.Schema({
+                                                username: { type: String, unique: true, required: true },
+                                                hash: { type: String, required: true },
+                                                firstName: { type: String, required: true },
+                                                lastName: { type: String, required: true },
+                                                createdDate: { type: Date, default: Date.now }
+
+                                            }, { collection: 'users'})
+
+module.exports = mongoose.model("User", userSchema)
