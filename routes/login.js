4fead202838cb4aac18292ffcfccd09b062c908d
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-26 21:50:05 +0200

    Create login.js

---
+++ routes/login.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+const the_controller = "../controllers/login/";
+
+
+const contr = [
+                {"name": "authenticate", "req": "post", "params": "", "middleware": "" },
+                {"name": "register", "req": "post", "params": "", "middleware": "" },
+                {"name": "getAll", "req": "get", "params": "/:id", "middleware": "" },
+                {"name": "current", "req": "get", "params": "/:id", "middleware": "" },
+                {"name": "getById", "req": "get", "params": "/:id", "middleware": ""},
+                {"name": "update", "req": "put", "params": "/:id", "middleware": ""},
+                {"name": "delete", "req": "delete", "params": "/:id", "middleware": ""}
+
+   ];
+
+/*
+                router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/current', getCurrent);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+                
+*/
+
+
+
+var i;
+
+for (i = 0; i < contr.length; i++) { 
+   
+    let contrName = contr[i]["name"];
+    let contrReq = contr[i]["req"];
+    let contrParams = contr[i]["params"];
+    let contrMiddleware = contr[i]["middleware"];  
+
+    // example: const the_controller = require("../controllers/form2/edit.js");
+    contr[contrName] = require(the_controller + contrName + ".js");
+
+    if(contrReq == "post") {
+      
+           // example = edit:  router.post('/edit', the_controller);
+           router.post('/' + contrName, contrMiddleware, contr[contrName][contrName]);
+
+    } else {
+      
+          // example = edit:  router.get('/edit/:id', the_controller);
+          router.get('/' + contrName + contrParams, contr[contrName][contrName]);
+
+    } // end if-else
+
+} // end for
+
+module.exports = router;
