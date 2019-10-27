d80885f21912463397c4adc1be6ca8500c6a35da
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-27 06:47:58 +0100

    Update authenticate.js

--- controllers/login/authenticate.js
+++ controllers/login/authenticate.js
@@ -1,1 +1,26 @@
-// bla
+var mainFolder = "../../";
+
+/*
+// Dependencies
+var cloudinary = require('cloudinary');
+
+// imports cloudinary config module module
+var configs = require(mainFolder + 'configs');
+
+var cloudyConfig = configs.cloudyConfig;
+
+cloudinary.config(cloudyConfig)
+*/
+
+// CREATES A RECORD (GET REQUEST)
+
+const theModel = require(mainFolder + 'models/cloudinary')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Authenticate User"
+var theUrl = "../contents/login/authenticate.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
