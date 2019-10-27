b6f84e6e7194b0e4d85917d0ec4efe6a62086f88
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-27 07:10:31 +0100

    Create register.js

---
+++ controllers/login/register.js
@@ -0,0 +1,53 @@
+var mainFolder = "../../";
+
+// imports jwt config module
+var configs = require(mainFolder + 'configs');
+var jwtConfig = configs.jwtConfig;
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+/*
+// Dependencies
+var cloudinary = require('cloudinary');
+// imports cloudinary config module module
+var configs = require(mainFolder + 'configs');
+var cloudyConfig = configs.cloudyConfig;
+cloudinary.config(cloudyConfig)
+*/
+
+// CREATES A RECORD (GET REQUEST)
+
+const theModel = require(mainFolder + 'models/user')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Authenticate User"
+var theUrl = "../contents/login/authenticate.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+async function createUsr(userParam) {
+    // validate
+    if (await theModel.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    const user = new User(userParam);
+
+    // hash password
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // save user
+    await user.save();
+}
+
+exports.register = function (req, res, next) {
+    createUsr(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
