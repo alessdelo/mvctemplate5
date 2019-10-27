c2fb68f6a36d3c4711ccc051f6c7425e2d138b86
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-27 06:54:36 +0100

    Update authenticate.js

--- controllers/login/authenticate.js
+++ controllers/login/authenticate.js
@@ -1,5 +1,12 @@
 var mainFolder = "../../";
 
+// imports jwt config module
+var configs = require(mainFolder + 'configs');
+var jwtConfig = configs.jwtConfig;
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 /*
 // Dependencies
 var cloudinary = require('cloudinary');
@@ -14,7 +21,7 @@ cloudinary.config(cloudyConfig)
 
 // CREATES A RECORD (GET REQUEST)
 
-const theModel = require(mainFolder + 'models/cloudinary')
+const theModel = require(mainFolder + 'models/user')
 
 var importVars = require(mainFolder + 'vars.js')
 var theIndex = importVars.index
@@ -24,3 +31,21 @@ var theUrl = "../contents/login/authenticate.ejs"
 var theDescr = "empty....."
 
 // -----------------------------------------
+
+async function authenticateUsr({ username, password }) {
+    const user = await theModel.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        const { hash, ...userWithoutHash } = user.toObject();
+        const token = jwt.sign({ sub: user.id }, jwtConfig.secret);
+        return {
+            ...userWithoutHash,
+            token
+        };
+    }
+}
+  
+exports.authenticate = function (req, res, next) {
+    authenticateUsr(req.body)
+        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
