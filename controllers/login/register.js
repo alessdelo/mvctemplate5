f40b026dff37747f61c4b49ea10a93652ea9bdeb
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-27 12:27:45 +0100

    Update register.js

--- controllers/login/register.js
+++ controllers/login/register.js
@@ -48,6 +48,7 @@ async function createUsr(userParam) {
 
 exports.register = function (req, res, next) {
     createUsr(req.body)
-        .then(() => res.json({}))
+        // .then(() => res.json({}))
+        .then(() => res.send(res)
         .catch(err => next(err));
 }
