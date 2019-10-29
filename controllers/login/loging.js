345ec3f794c2383e9faa9f1ad2c8d7c733923890
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-29 14:36:38 +0100

    Update loging.js

--- controllers/login/loging.js
+++ controllers/login/loging.js
@@ -29,7 +29,7 @@ exports.loging = function (req, res) {
                 }
         
         // debug
-        // res.send(text)
-        res.render(theIndex.index,text)
+        res.send(text)
+        // res.render(theIndex.index,text)
         
 } // end exports.
