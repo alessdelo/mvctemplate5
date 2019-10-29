ff830474dee99a8cfd56685835ce8f8c0f4bec08
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-29 07:15:35 +0100

    Create registerg.js

---
+++ controllers/login/registerg.js
@@ -0,0 +1,35 @@
+// CREATES A RECORD (GET REQUEST)
+
+var mainFolder = "../../";
+
+// const theModel = require(mainFolder + 'models/maps')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Register Form"
+var theUrl = "../contents/login/registerg.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+// change this!!!!!!
+exports.register = function (req, res) {
+
+        var text =  {
+                 title: theTitle, 
+                 content:theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: []
+                }
+        
+        // debug
+        // res.send(text)
+        res.render(theIndex.index,text)
+        
+} // end exports.
