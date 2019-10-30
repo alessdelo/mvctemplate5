a70516214da039bc4d9b130c12da9405a96e9a09
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-10-30 14:19:21 +0100

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,67 @@
 const express = require('express');
 const router = express.Router();
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+const the_controller = "../controllers/login/";
+
+
+const contr = [
+  
+                {"name": "registerg", "req": "get", "params": "", "middleware": ""},
+                {"name": "loging", "req": "get", "params": "", "middleware": ""},
+                {"name": "authenticate", "req": "post", "params": "", "middleware": ""},
+                {"name": "register", "req": "post", "params": "", "middleware": "" }
+  
+   ];
+
+/*
+                {"name": "authenticate", "req": "post", "params": "", "middleware": "" },
+                {"name": "register", "req": "post", "params": "", "middleware": "" },
+                {"name": "getAll", "req": "get", "params": "/:id", "middleware": "" },
+                {"name": "current", "req": "get", "params": "/:id", "middleware": "" },
+                {"name": "getById", "req": "get", "params": "/:id", "middleware": ""},
+                {"name": "update", "req": "put", "params": "/:id", "middleware": ""},
+                {"name": "delete", "req": "delete", "params": "/:id", "middleware": ""}
+*/
+
+
+var i;
+
+for (i = 0; i < contr.length; i++) { 
+   
+    let contrName = contr[i]["name"];
+    let contrReq = contr[i]["req"];
+    let contrParams = contr[i]["params"];
+    let contrMiddleware = contr[i]["middleware"];  
+
+    // example: const the_controller = require("../controllers/form2/edit.js");
+    contr[contrName] = require(the_controller + contrName + ".js");
+
+    if(contrReq == "post") {
+      
+           // example = edit:  router.post('/edit', the_controller);
+           router.post('/' + contrName, contrMiddleware, contr[contrName][contrName]);
+
+    } else {
+      
+          // example = edit:  router.get('/edit/:id', the_controller);
+          router.get('/' + contrName + contrParams, contr[contrName][contrName]);
+
+    } // end if-else
+
+} // end for
+
+module.exports = router;
+
+
+
+
+
+
+/*
+const express = require('express');
+const router = express.Router();
 
 const the_controller = "../controllers/login/";
 
@@ -11,7 +73,7 @@ const contr = [
 
               ];
 
-
+*/
 /*
                 {"name": "authenticate", "req": "post", "params": "", "middleware": "" },
                 {"name": "register", "req": "post", "params": "", "middleware": "" },
@@ -23,6 +85,8 @@ const contr = [
                 
 */
 
+
+/*
 var i;
 
 for (i = 0; i < contr.length; i++) { 
@@ -49,3 +113,4 @@ for (i = 0; i < contr.length; i++) {
 } // end for
 
 module.exports = router;
+*/
