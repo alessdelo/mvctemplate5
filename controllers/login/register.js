2b6f7c01c79f5ab8990af8c5a93e0a78cb2a0816
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-01 09:27:02 +0100

    Update register.js

--- controllers/login/register.js
+++ controllers/login/register.js
@@ -96,6 +96,7 @@ exports.register = function (req, res, next) {
         // .then(data => return {res.send(data); })
         // .then(function(data) {res.send(data)})
         .then(data => res.send("the data are these: " + data))
+        .then(data => res.send("the data are these: " + JSON.stringify(data)))
         // .then(() => res.json({}))
         // .then(data => res.send.bind(data))
         // .then(data => res.json({data}))
