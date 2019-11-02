4a148de31f8c4c7eb42c852ca14fddf48a5ad939
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-02 06:24:23 +0100

    Update register.js

--- controllers/login/register.js
+++ controllers/login/register.js
@@ -111,13 +111,13 @@ async function testFunct(params) {
 
 
     var text =  {
-                       text[title]: "",                       
-                       text[content]: "",
-                       text[descr]: "",
-                       text[nav]: "",
-                       text[header]: "",
-                       text[footer]: "",
-                       text[params]: ""
+                       title: "",                       
+                       content: "",
+                       descr: "",
+                       nav: "",
+                       header: "",
+                       footer: "",
+                       params: ""
 };
 
     // save user
