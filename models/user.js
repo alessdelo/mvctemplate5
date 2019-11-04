9f48a22f0cd189c8d9da74a843e567725be27249
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-04 14:00:49 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose")
 // a very basic mongoose schema for locations
 var userSchema = new mongoose.Schema({
                                                 username: { type: String, unique: true, required: true },
+                                                email: { type: String, unique: true, required: true },
                                                 hash: { type: String, required: true },
                                                 firstName: { type: String, required: true },
                                                 lastName: { type: String, required: true },
