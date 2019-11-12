b2fcb504d188e0c803148886f3e21910d3236c5e
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-12 07:01:32 +0100

    Update deletep.js

--- controllers/login/deletep.js
+++ controllers/login/deletep.js
@@ -1,10 +1,10 @@
 var mainFolder = "../../";
 
+/*
 // Dependencies
  var cloudinary = require('cloudinary');
 
 // var cloudinary = require('cloudinary').v2
-
 
 // imports cloudinary config module module
 var configs = require(mainFolder + 'configs');
@@ -12,38 +12,33 @@ var configs = require(mainFolder + 'configs');
 var cloudyConfig = configs.cloudyConfig;
 
 cloudinary.config(cloudyConfig)
+*/
 
 
-const theModel = require(mainFolder + 'models/cloudinary')
+const theModel = require(mainFolder + 'models/user')
 // const debugs = require(mainFolder + 'helpers/debugs.js')
 // const randoms = require(mainFolder + 'helpers/randoms.js')
 
 var importVars = require(mainFolder + 'vars.js')
 var theIndex = importVars.index
 
-var theTitle = "Cloudinary: just deleted record"
-var theUrl = "../contents/cloudinary/deletep.ejs"
+var theTitle = "Just deleted record"
+var theUrl = "../contents/login/deletep.ejs"
 var theDescr = "empty....."
 
 // -----------------------------------------
 
-exports.deletep = function (req, res) {
+exports.deletep = async function (req, res) {
 
   
 
 
 
-    theModel.findByIdAndRemove(req.body._id, { useFindAndModify: false }, function (err, result) {
+   await theModel.findByIdAndRemove(req.body._id, { useFindAndModify: false }, function (err, result) {
         if (err) return next(err)
         // res.send(result)
         // res.send('item id: ' + req.body.id + ' Deleted successfully!')
-        // res.send( debugs.textFromObject(result) )
-
-
-
-
-        cloudinary.uploader.destroy(result['image_id'], function(clError,clResult) {
-     
+        // res.send( debugs.textFromObject(result) )   
                                                      
                                                     var theParams = {"result": result,
                                                                      "clResult": clResult
@@ -64,8 +59,6 @@ exports.deletep = function (req, res) {
                                                     
                                                     res.render(theIndex.index,text)
 
-
-    })  // end cloudinary
 
   }) // end findByIdAndRemove
 
