52cc021f5dc57cb489c5aa9a2718962c16352d6a
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-19 07:34:22 +0100

    Update updatep.js

--- controllers/login/updatep.js
+++ controllers/login/updatep.js
@@ -71,3 +71,56 @@ exports.updatep = function (req, res) {
 
 
 } // end update
+
+
+
+// ----------------------------------------
+
+/*
+
+exports.updatep = function (req, res) {
+
+    var theReq = req.body              
+    
+    theModel.findByIdAndUpdate(
+                            { "_id": theReq._id },
+        
+                            { $set: { "username": theReq.username,
+                                      // "password": theReq.password,
+                                      "firstName": theReq.firstName,
+                                      "lastName": theReq.lastName
+                                    }
+                            }, 
+           
+                            {new: true},
+    
+                            function (err, result) {
+
+                            if (err)  res.send(err)                            
+                            // if (err) return next(new Error('There was an error'))
+
+                                                    // debug
+                                                    // res.send(req.body)
+                                                    var theParams = theReq
+                                                           
+                                                    var text =  {
+                                                                 title: theTitle, 
+                                                                 content:theUrl,
+                                                                 descr: theDescr,
+                                                                 nav: theIndex.nav,
+                                                                 header: theIndex.header,
+                                                                 footer: theIndex.footer,
+                                                                 params: [theParams]
+                                                                }
+                                                    // debug
+                                                    // res.send(text)
+                                                   
+                                                    res.render(theIndex.index,text)
+
+
+                            } )
+
+
+} // end update
+
+*/
