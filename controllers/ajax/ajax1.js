d04c547874a579df4ff23cb4772cc6ce8bae202e
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-20 13:52:51 +0100

    Update ajax1.js

--- controllers/ajax/ajax1.js
+++ controllers/ajax/ajax1.js
@@ -1,1 +1,37 @@
-//---
+var mainFolder = "../../";
+
+// const theModel = require(mainFolder + 'models/maps')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Jquery tests"
+var theUrl = "../contents/ajax/ajax1.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+exports.ajax1 = function (req, res) {
+  
+    
+       
+       var theParams = {
+                 
+                 }
+           
+        var text =  {
+                 title: theTitle, 
+                 content: theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+        
+        // debug
+        //res.send(text)
+        
+        res.render(theIndex.index,text)
+
+} // end export.
