133ab633885895d42dcf51607e4121797294c0a3
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-21 07:12:37 +0100

    Update ajax1Test.js

--- controllers/ajax/ajax1Test.js
+++ controllers/ajax/ajax1Test.js
@@ -3,6 +3,6 @@ exports.ajax1Test = function (req, res) {
            
         var text = "aaaa";
  
-        res.status(200).send(text)
+        res.status(200).json({ message: text });
         
 } // end export.
