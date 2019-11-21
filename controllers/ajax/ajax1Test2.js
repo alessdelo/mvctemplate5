88bbeab6a8a6d77db227766ff4a39be6b4092ae0
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-21 15:14:30 +0100

    Update ajax1Test2.js

--- controllers/ajax/ajax1Test2.js
+++ controllers/ajax/ajax1Test2.js
@@ -1,3 +1,12 @@
+/*
 app.get('/searching', function(req, res){
  res.send("WHEEE");
 });
+*/
+
+
+exports.ajax1Test2 = function (req, res) {
+
+        res.send("WHEEE");
+        
+} // end export.
