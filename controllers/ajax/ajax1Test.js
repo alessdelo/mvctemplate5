77fc948dfba55778721506834ec4f0b1557dfff5
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-11-22 05:50:55 +0100

    Update ajax1Test.js

--- controllers/ajax/ajax1Test.js
+++ controllers/ajax/ajax1Test.js
@@ -4,5 +4,6 @@ exports.ajax1Test = function (req, res) {
         var text = "aaaa";
  
         res.status(200).json({ message: text });
+        console.log("ajax recieved!");
         
 } // end export.
