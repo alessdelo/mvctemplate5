4d5c48991c2c6157e3c2a45a67528ae57ac681f1
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-12-07 17:03:48 +0100

    Update mosca1broker.js

--- controllers/moscaMQTT/mosca1broker.js
+++ controllers/moscaMQTT/mosca1broker.js
@@ -1,14 +1,6 @@
-
-
-
-
-// CREATES A RECORD (GET REQUEST)
-
 var mainFolder = "../../";
 
-// const theModel = require(mainFolder + 'models/kitty')
-// const debugs = require(mainFolder + 'helpers/debugs.js')
-// const randoms = require(mainFolder + 'helpers/randoms.js')
+// const theModel = require(mainFolder + 'models/maps')
 
 var importVars = require(mainFolder + 'vars.js')
 var theIndex = importVars.index
@@ -19,16 +11,39 @@ var theDescr = "empty....."
 
 // -----------------------------------------
 
+exports.mosca1broker = function (req, res) {
+  
+    
+       
+       var theParams = {
+                 
+                 }
+           
+        var text =  {
+                 title: theTitle, 
+                 content: theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+        
+        // debug
+        res.send(text)
+        
+       // res.render(theIndex.index,text)
+
+} // end export.
+
+
+
 /*
 // mosca MQTT broker
 var mosca = require("mosca")
 var settings = {port: 1234}
 var broker = new mosca.Server(settings)
 */
-
-exports.mosca1broker = function (req, res) {
-  
-          res.send('test broker')
 
         /*
        broker.on('ready', () => {
@@ -37,39 +52,5 @@ exports.mosca1broker = function (req, res) {
            // res.render(theIndex.index,text)
         }) 
         */
- /* 
-    let theSchema = new theModel(
-        {
-            name: req.params.name,
-            age: req.params.age
-            
-        }
-    )
 
-    theSchema.save(function (err) {
-        if (err) {
-            return next(err)
-        }
-        
-        var theParams = {
-                 "name": req.params.name,
-                 "age": req.params.age
-                 };
 
-        var text =  {
-                 title: theTitle, 
-                 content:theUrl,
-                 descr: theDescr,
-                 nav: theIndex.nav,
-                 header: theIndex.header,
-                 footer: theIndex.footer,
-                 params: [theParams]
-                }
-
-        res.render(theIndex.index,text)
-        
-    }) // end save
-
-*/
-
-} // end .export
