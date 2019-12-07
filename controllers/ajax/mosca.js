fbf361fc8753391018878bbe8918febf348a475e
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-12-07 17:26:28 +0100

    Update mosca.js

--- controllers/ajax/mosca.js
+++ controllers/ajax/mosca.js
@@ -1,5 +1,27 @@
+
+// mosca MQTT broker
+var mosca = require("mosca")
+var settings = {port: 1234}
+var broker = new mosca.Server(settings)
+
+
+        /*
+       broker.on('ready', () => {
+           console.log('broker is ready!')
+           res.send('broker is ready!')
+           // res.render(theIndex.index,text)
+        }) 
+        */
+
+
 exports.mosca = function (req, res) {
             
-       res.send("hello mosca!");
+       // res.send("hello mosca!");
+       broker.on('ready', () => {
+           console.log('broker is ready!')
+           res.send('broker is ready!')
+           // res.render(theIndex.index,text)
+        })             
+            
         
 } // end export.
