4bc08b0609cb1b5709f123ced0c38b0fcf1cc3f7
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-12-07 11:22:53 +0100

    Update mosca1broker.js

--- controllers/moscaMQTT/mosca1broker.js
+++ controllers/moscaMQTT/mosca1broker.js
@@ -1,9 +1,71 @@
+
+
+
+
+// CREATES A RECORD (GET REQUEST)
+
+var mainFolder = "../../";
+
+const theModel = require(mainFolder + 'models/kitty')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Form2 (Create)"
+var theUrl = "../contents/form2/createg.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
 // mosca MQTT broker
 var mosca = require("mosca")
 var settings = {port: 1234}
 var broker = new mosca.Server(settings)
 
-broker.on('ready', () => {
-  console.log('broker is ready!')
-})
+exports.mosca1broker = function (req, res) {
 
+  
+       broker.on('ready', () => {
+           console.log('broker is ready!')
+           res.send('broker is ready!')
+           // res.render(theIndex.index,text)
+        }) 
+
+ /* 
+    let theSchema = new theModel(
+        {
+            name: req.params.name,
+            age: req.params.age
+            
+        }
+    )
+
+    theSchema.save(function (err) {
+        if (err) {
+            return next(err)
+        }
+        
+        var theParams = {
+                 "name": req.params.name,
+                 "age": req.params.age
+                 };
+
+        var text =  {
+                 title: theTitle, 
+                 content:theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+
+        res.render(theIndex.index,text)
+        
+    }) // end save
+
+*/
+
+} // end .export
