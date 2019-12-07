aeb9be7ca3ac2341eac5b53baa2654e4121527ac
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-12-07 10:17:32 +0100

    Create ajax1Test4.js

---
+++ controllers/ajax/ajax1Test4.js
@@ -0,0 +1,11 @@
+exports.ajax1Test4 = function (req, res) {
+
+      var data = {
+                    firstName: "Franco",
+                    lastName: "Franchi"    
+                  };
+       
+       
+       res.send("WHEEE");
+        
+} // end export.
