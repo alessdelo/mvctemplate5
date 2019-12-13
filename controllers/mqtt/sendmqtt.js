f1f8a6181942ac13510224caacee4d02f5fd98f1
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-12-13 15:06:50 +0100

    Update sendmqtt.js

--- controllers/mqtt/sendmqtt.js
+++ controllers/mqtt/sendmqtt.js
@@ -1,1 +1,37 @@
-// aaaa
+var mainFolder = "../../";
+
+// const theModel = require(mainFolder + 'models/maps')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "MQTT test n.1"
+var theUrl = "../contents/mqtt/sendmqtt.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+exports.sendmqtt = function (req, res) {
+  
+    
+       
+       var theParams = {
+                 
+                 }
+           
+        var text =  {
+                 title: theTitle, 
+                 content: theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+        
+        // debug
+        // res.send(text)
+        
+       res.render(theIndex.index,text)
+
+} // end export.
