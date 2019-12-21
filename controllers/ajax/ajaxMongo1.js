d9394d658a2381bfe35f45fa907b7ea25bbb139b
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-12-21 17:04:28 +0100

    Update ajaxMongo1.js

--- controllers/ajax/ajaxMongo1.js
+++ controllers/ajax/ajaxMongo1.js
@@ -3,40 +3,17 @@
 var mainFolder = "../../";
 
 const theModel = require(mainFolder + 'models/kitty')
-// const debugs = require(mainFolder + 'helpers/debugs.js')
-// const randoms = require(mainFolder + 'helpers/randoms.js')
-
-var importVars = require(mainFolder + 'vars.js')
-var theIndex = importVars.index
-
-var theTitle = "Records Table"
-var theUrl = "../contents/form2/recordlist.ejs"
-var theDescr = "empty....."
 
 // -----------------------------------------
 
-exports.recordlist = function (req, res) {
+exports.ajaxMongo1 = function (req, res) {
 
     theModel.find({}, { useFindAndModify: false }, function (err, result) {
     
-         if (err) res.send(debugs.textFromObject(err)) // return next(err)
-                  
-                var theParams = {result}
-       
-                var text =  {
-                         title: theTitle, 
-                         content: theUrl,
-                         descr: theDescr,
-                         nav: theIndex.nav,
-                         header: theIndex.header,
-                         footer: theIndex.footer,
-                         params: [theParams]
-                        }
-                
-                res.render(theIndex.index,text)
-                
-                // res.send("the num: " + req.params.num + "<br> result: " + debugs.textFromObject(result))
+         if (err) res.send(err) // return next(err)
+        
+                res.send(result)
 
-            } ).sort({$natural:1}).limit(parseInt(req.params.num))
+            } ).sort({$natural:1}).limit(10)
     
- } // end lastitems
+ } // end ajaxMongo1
