787e64be3e016f0913fa6dfaa78060ee5a19833e
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-12-22 12:46:09 +0100

    Update ajax.js

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -151,3 +151,51 @@ EXAMPLE
 
 */
 
+
+*/
+
+// **********************************************
+// **********************************************
+
+
+// AJAX CALL WITH FUNCTION PARAMETER
+// basic ajax function to show json data into a html div on click
+
+function ajaxFunctionParameter(thingToClick, theUrl, theTarget) {
+                
+      $(thingToClick).click(function() {		    
+			
+             $.ajax({
+		      url: theUrl,
+		      type: 'get',
+		      dataType:'json',
+		      success: function(data) {
+                                                $(theTarget).html(data); 
+                                              },
+		      error: function() {
+					     alert(error);
+                                         }		                    
+              }); // end $.ajax
+						
+       }); // end .click function
+
+} // end ajaxJsonToDiv  
+
+/*
+++++++++++++
+INSTRUCTIONS
+++++++++++++
+thingToClick = the HTML element to click (button, div, a...)
+url = the url with the ajax data (can be even an express route)
+theTarget = html div that will show the data
++++++++
+EXAMPLE
++++++++
+<button id="theThingToClick">click to test</button>
+<div id="theTargetDiv">waiting for ajax text...</div>
+			
+<script>
+    // recalls the ajax function
+    ajaxJsonToDiv("#theThingToClick", "/test/test.txt", "#theTargetDiv");
+</script>
+*/
