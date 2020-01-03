cb9b8fed44d857c5372e1db7688476e2220d8c7c
alessdelo <33528892+alessdelo@users.noreply.github.com>
2020-01-03 19:31:08 +0100

    Update ajaxPostResp.js

--- controllers/ajax/ajaxPostResp.js
+++ controllers/ajax/ajaxPostResp.js
@@ -1,8 +1,8 @@
 exports.ajaxPostResp = function (req, res) {
   
-    // res.send("this is a post response!");   
+    res.send("this is a post response!");   
     // res.send("the request is: " + JSON.stringify(req.body));
-  
+    /*
     console.log('Post a Customer: ' + JSON.stringify(req.body));
     var customer = {};
     customer.firstname = req.body.firstname;
@@ -11,5 +11,5 @@ exports.ajaxPostResp = function (req, res) {
     customers.push(customer);
   
     return res.send(customer);
-      
+      */
 } // end ajaxPosResp
