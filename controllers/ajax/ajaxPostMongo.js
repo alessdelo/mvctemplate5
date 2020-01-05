65b2715e430a38ba440c51e1f1f58d9367578c9f
alessdelo <33528892+alessdelo@users.noreply.github.com>
2020-01-05 20:53:53 +0100

    Update ajaxPostMongo.js

--- controllers/ajax/ajaxPostMongo.js
+++ controllers/ajax/ajaxPostMongo.js
@@ -8,14 +8,20 @@ const theModel = require(mainFolder + 'models/cloudinary')
 
 exports.ajaxPostMongo = function (req, res) {
   
+  /*
+  
     // {title: /^t/} filters with a regex all the records in witch "title" field starts with the letter sent by POST frpm the form
     theModel.find({title: /^a/}, { useFindAndModify: false }, function (err, result) {
     
-         if (err) res.send(err) // return next(err)
+                if (err) res.send(err) // return next(err)
         
                 res.send(result)
 
             } ).sort({$natural:1}).limit(10)
+            
+    */
+  
+  res.send(req.body);
     
  } // end ajaxCloudinary 
 
