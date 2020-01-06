9bb136bf17654daf2ad4e2e17d9a5daa1e294fb4
alessdelo <33528892+alessdelo@users.noreply.github.com>
2020-01-06 11:46:08 +0100

    Update ajaxPostMongo.js

--- controllers/ajax/ajaxPostMongo.js
+++ controllers/ajax/ajaxPostMongo.js
@@ -8,7 +8,7 @@ const theModel = require(mainFolder + 'models/cloudinary')
 
 exports.ajaxPostMongo = function (req, res) {
   
-    var theRegex = new RegExp("/^" + "G" + "/");
+    var theRegex = new RegExp("^" + "G"); // equivalent to /^G/
   
     console.log('req.body: ' + JSON.stringify(req.body));
   
