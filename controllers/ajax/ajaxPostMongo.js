42648f521531f0808ece48953640dded2160f7cd
alessdelo <33528892+alessdelo@users.noreply.github.com>
2020-01-06 11:59:12 +0100

    Update ajaxPostMongo.js

--- controllers/ajax/ajaxPostMongo.js
+++ controllers/ajax/ajaxPostMongo.js
@@ -6,13 +6,24 @@ const theModel = require(mainFolder + 'models/cloudinary')
 
 // -----------------------------------------
 
+ // REGEX QUERY
+ // all:     theModel.find({}, .....}
+ // item name into field:     theModel.find({myField:{$regex:"myName"}}, .....}
+ // field item starts with one or more letters:   theModel.find({"title": {$regex: /^G/, $options: 'i'}}, .....}
+ //       - G is a letter (or a string)
+ //       - "i" the option that accept "case insensitivity"
+  
+ // function new RegExp("^" + "G");
+ //       - trasform a string in a regular expression
+ //       - can dynamically insert into the regex a string passed from a form
+ //       - you must exclude and the     
+
 exports.ajaxPostMongo = function (req, res) {
   
     var theRegex = new RegExp("^" + "G"); // equivalent to /^G/
   
     console.log('req.body: ' + JSON.stringify(req.body));
-  
-    // {title: /^t/} filters with a regex all the records in witch "title" field starts with the letter sent by POST frpm the form
+ 
     theModel.find({"title": {$regex: theRegex, $options: 'i'}}, { useFindAndModify: false }, function (err, result) {
     
                 if (err) res.send(err) // return next(err)
@@ -20,16 +31,5 @@ exports.ajaxPostMongo = function (req, res) {
                 res.send(result)
 
             } ).sort({$natural:1}).limit(10)
-            
- // REGEX QUERY
- // all:     theModel.find({}, .....}
- // item name into field:     theModel.find({myField:{$regex:"myName"}}, .....}
- // field item starts with one or more letters:   theModel.find({"title": {$regex: /^G/, $options: 'i'}}, .....}
- //      - G is a letter (or a string)
- //      - "i" the option that accept "case insensitivity"
-  
-  // theModel.find({title: /^a/}, { useFindAndModify: false }, function (err, result) {
- // res.send("postmongo: " + req.body);
-    
+                
  } // end ajaxCloudinary 
-
