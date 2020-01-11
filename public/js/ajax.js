53b9151ab794539b1ed14af5235f044214be3e1c
alessdelo <33528892+alessdelo@users.noreply.github.com>
2020-01-12 00:19:28 +0100

    Update ajax.js

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -512,7 +512,7 @@ function ajaxForm0(theUrl, formID, formData, customfunction) {
                                                       break;
 
                                                       case "checkbox":
-							    $('input[name=' + key + ']).prop('checked',false);
+							    $('input[name=' + key + ']').prop('checked',false);
                                                       break;
 
                                                       default:
