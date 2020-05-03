ed0be77c94192e4cf29bde72eb0fc9486d2ee336
alessdelo <33528892+alessdelo@users.noreply.github.com>
2020-05-03 15:38:24 +0200

    Update base.js

--- controllers/aframe/base.js
+++ controllers/aframe/base.js
@@ -1,1 +1,37 @@
-aaa
+var mainFolder = "../../";
+
+// const theModel = require(mainFolder + 'models/maps')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Aframe Base"
+var theUrl = "../contents/aframe/base.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+exports.aframeBase = function (req, res) {
+  
+    
+       
+       var theParams = {
+                 
+                 }
+           
+        var text =  {
+                 title: theTitle, 
+                 content: theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: [theParams]
+                }
+        
+        // debug
+        // res.send(text)
+        
+       res.render(theIndex.index,text)
+
+} // end export.
