f7728eb0f1f9ea21bdb997d20169376416a6c991
alessdelo <33528892+alessdelo@users.noreply.github.com>
2021-02-09 07:29:02 +0100

    Update form.js

--- controllers/dapp1/form.js
+++ controllers/dapp1/form.js
@@ -1,1 +1,35 @@
-// bla
+// CREATES A RECORD (GET REQUEST)
+
+var mainFolder = "../../";
+
+// const theModel = require(mainFolder + 'models/maps')
+// const debugs = require(mainFolder + 'helpers/debugs.js')
+// const randoms = require(mainFolder + 'helpers/randoms.js')
+
+var importVars = require(mainFolder + 'vars.js')
+var theIndex = importVars.index
+
+var theTitle = "Cloudinary Form 1 (Upload)"
+var theUrl = "../contents/cloudinary/form1.ejs"
+var theDescr = "empty....."
+
+// -----------------------------------------
+
+// change this!!!!!!
+exports.form1 = function (req, res) {
+
+        var text =  {
+                 title: theTitle, 
+                 content:theUrl,
+                 descr: theDescr,
+                 nav: theIndex.nav,
+                 header: theIndex.header,
+                 footer: theIndex.footer,
+                 params: []
+                }
+        
+        // debug
+        // res.send(text)
+        res.render(theIndex.index,text)
+        
+} // end exports.
