3c2d6e156d511e9cc5d6f92f33e51245d0e8fb40
alessdelo <33528892+alessdelo@users.noreply.github.com>
2021-02-16 14:42:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 
 // Imports routes for every content
+/*
 const home = require('./routes/home')
 const basics = require('./routes/basics')
 const home2 = require('./routes/home2')
@@ -53,7 +54,7 @@ const theAjax = require('./routes/ajax')
 const theMosca = require('./routes/mosca')
 const theMQTT = require('./routes/mqtt')
 const aframe = require('./routes/aframe')
-
+*/
 const dapp1 = require('./routes/dapp1')
 
 // helpers
@@ -90,6 +91,7 @@ var app = express()
 
  // app.use(bodyParser.json())
  // app.use(bodyParser.urlencoded({extended: false}))
+/*
   app.use('/', home)
   app.use('/home', home)
   app.use('/basics', basics)
@@ -110,6 +112,7 @@ var app = express()
   app.use('/mosca', theMosca)
   app.use('/mqtt', theMQTT)
   app.use('/aframe', aframe)
+  */
   app.use('/dapp1', dapp1)
 
 // temporary ajax test
