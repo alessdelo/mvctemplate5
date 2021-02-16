b1e8c8829710a33661ee93b422d70eaffc78acb6
alessdelo <33528892+alessdelo@users.noreply.github.com>
2021-02-16 14:55:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const path = require('path')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const mqtt = require('mqtt')
 const Web3 = require('web3')
 
